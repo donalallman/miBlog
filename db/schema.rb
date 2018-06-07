01518a70eeb5a4594990092ff6ae6a06716623e8
Donal Allman <donal10.gmail.com>
2018-06-07 18:12:35 +0200

    autores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606135819) do
+ActiveRecord::Schema.define(version: 20180607160451) do
 
   create_table "articulos", force: :cascade do |t|
     t.string "titulo"
@@ -24,10 +24,20 @@ ActiveRecord::Schema.define(version: 20180606135819) do
   end
 
   create_table "autors", force: :cascade do |t|
-    t.string "autor"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_autors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_autors_on_reset_password_token", unique: true
   end
 
   create_table "comentarios", force: :cascade do |t|
